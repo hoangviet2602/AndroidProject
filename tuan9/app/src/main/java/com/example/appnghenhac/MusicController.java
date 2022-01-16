43d010f63d6d16b2422f4dd467734bc2e624caed
toan2310 <nguyenquoctoan0709@gmail.com>
2022-01-16 13:51:00 +0700

    toan commit profile user

---
+++ tuan9/app/src/main/java/com/example/appnghenhac/MusicController.java
@@ -0,0 +1,10 @@
+package com.example.appnghenhac;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController {
+    public MusicController(Context context) {
+        super(context);
+    }
+}
